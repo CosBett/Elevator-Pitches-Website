7a1c8962611278556009b44410d5dd7f5459dff0
CosBett <cosmasbett9@gmail.com>
2022-02-07 17:43:19 +0300

    create configuration for development staging and production

--- config/base_config.py
+++ config/base_config.py
@@ -1,6 +1,25 @@
-from flask import Flask
-
+import secrets
 
 class Base:
   FLASK_APP = 'main.py'
   SQLACHEMY_TRACK_MODIFICATION = False
+  SECRET_KEY = secrets.token_hex(16)
+
+class Development(Base):
+  FLASK_ENV = 'development'
+  DATABASE = ''
+  POSTGRES_USER = ''
+  POSTGRES_PASSWORD = ''
+  SQLALCHEMY_DATABASE_URL = 'sqlite://tmp/elevatorPitch.db'
+
+class Staging(Base):
+  DATABASE = ''
+  POSTGRES_USER = ''
+  POSTGRES_PASSWORD = ''
+  SQLALCHEMY_DATABASE_URL = 'sqlite://tmp/elevatorPitch.db'
+
+class Production(Base):
+  DATABASE = ''
+  POSTGRES_USER = ''
+  POSTGRES_PASSWORD = ''
+  SQLALCHEMY_DATABASE_URL = 'sqlite://tmp/elevatorPitch.db'
