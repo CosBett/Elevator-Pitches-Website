d1d1cd609696b3f8bc34ac99c10172669ef12423
CosBett <cosmasbett9@gmail.com>
2022-02-07 12:44:24 +0300

    create sign out endpoint

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ def signup():
 
 # Sign In endpoint
 def signin():
-  return 'sign in working' 
+  return'sign in working' 
 # Sign Out endpoint
-
+def signout():
+  return 'sign out working'
 
 #Setting debug Mode to true
 if __name__=='__main__':
