77525e7dae66cbda5b00c14f6667d131bc18ae2e
CosBett <cosmasbett9@gmail.com>
2022-02-07 12:33:42 +0300

    add application endpoint

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from flask import Flask
 
 # Create instance of my application 
 app = Flask(__name__)
+
+# Add app endpoints
+@app.route('/')
+
+# View function
+def homepage():
+   return 'IT IS WORKING'
+   
+#Setting debug Mode to true
+if __name__=='__main__':
+  app.run(debug=True)
